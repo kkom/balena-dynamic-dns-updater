d76c5695e7f5ea551c19cdcb865289e1e8ba175e
Konrad Komorowski <konrad@komorowski.me>
2018-09-14 00:20:41 +0100

    nits in the configuration container

--- configure/src/run.sh
+++ configure/src/run.sh
@@ -3,6 +3,8 @@
 # Exit if any command fails
 set -e
 
+# Replace values in the configuration file
 sh replace.sh ddclient.conf.template > /config/ddclient.conf
 
+# Print the configuration file for debugging purposes
 cat /config/ddclient.conf
